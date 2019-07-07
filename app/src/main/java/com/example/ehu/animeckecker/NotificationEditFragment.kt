1a5b157145312bac21ce2b1f09f9aa207c35bb64
ehu <fehuehuf@gmail.com>
2019-07-08 03:38:02 +0900

    navigationで通知設定画面に遷移

---
+++ app/src/main/java/com/example/ehu/animeckecker/NotificationEditFragment.kt
@@ -0,0 +1,23 @@
+package com.example.ehu.animeckecker
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.ehu.animeckecker.databinding.FragmentNotificationEditBinding
+
+
+class NotificationEditFragment : Fragment() {
+    lateinit var binding: FragmentNotificationEditBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_notification_edit, container, false)
+        return binding.root
+    }
+}
