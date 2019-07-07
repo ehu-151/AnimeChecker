7e7fc58f3f021951f7177120754dfab30ae7aa39
ehu <fehuehuf@gmail.com>
2019-07-08 03:04:49 +0900

    navigationで画面遷移を実装

--- app/src/main/java/com/example/ehu/animeckecker/MyNotificationFragment.kt
+++ app/src/main/java/com/example/ehu/animeckecker/MyNotificationFragment.kt
@@ -2,11 +2,12 @@ package com.example.ehu.animeckecker
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.ehu.animeckecker.databinding.FragmentMyNotificationBinding
 
 class MyNotificationFragment : Fragment() {
@@ -16,6 +17,9 @@ class MyNotificationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_notification, container, false)
+        binding.addNotification.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_myNotificationFragment_to_thisSeasonFragment)
+        }
         return binding.root
     }
 
