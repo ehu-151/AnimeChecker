c33b54dd32bade69d459bd91b20f55226161d9fa
ehu <fehuehuf@gmail.com>
2019-07-07 10:39:19 +0900

    client_idの更新

--- app/src/main/java/com/example/ehu/animeckecker/LoginActivity.kt
+++ app/src/main/java/com/example/ehu/animeckecker/LoginActivity.kt
@@ -3,34 +3,37 @@ package com.example.ehu.animeckecker
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.browser.customtabs.CustomTabsIntent
 import androidx.databinding.DataBindingUtil
 import com.example.ehu.animeckecker.databinding.ActivityLoginBinding
 import android.content.Intent
 
 
-
-
 class LoginActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityLoginBinding
-    val base_url = "https://annict.com/oauth/authorize"
-    val client_id = "?client_id=jBqFV693ClvBF2bhIUJtzwr2WDlGmLf7YRWMlKBiU4I"
-    val redirect_url = "&redirect_uri=urn:ietf:wg:oauth:2.0:oob"
-    val response_type = "&response_type=code"
+    val baseUrl = "https://annict.com/oauth/authorize"
+    val clientId = "?client_id=NqicoWdMY8vempcSSBVJ5K5Z8pN6HoIqKPBxB2DG6DM"
+    val redirectUrl = "&redirect_uri=urn:ietf:wg:oauth:2.0:oob"
+    val responseType = "&response_type=code"
     val scope = "&scope=read"
+    val clientSecret="kiAVRykpxkSpa83qvrXDxLn7wiu2j5Ja9lKbGFHZLHc"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         binding.launchAuthorize.setOnClickListener { launchBrowser() }
+        binding.login.setOnClickListener { login() }
     }
 
     private fun launchBrowser() {
-        val uri = Uri.parse(base_url + client_id + redirect_url + response_type + scope)
+        val uri = Uri.parse(baseUrl + clientId + redirectUrl + responseType + scope)
         val i = Intent(Intent.ACTION_VIEW, uri)
         startActivity(i)
+    }
+
+    private fun login() {
+        val code = binding.codeText.text
     }
 
 }
