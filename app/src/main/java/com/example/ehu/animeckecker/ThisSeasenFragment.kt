f90cf813fe4575a01973bdd5095cf3758d0d1e57
ehu <fehuehuf@gmail.com>
2019-07-07 19:18:21 +0900

    fragmentの画面遷移

---
+++ app/src/main/java/com/example/ehu/animeckecker/ThisSeasenFragment.kt
@@ -0,0 +1,32 @@
+package com.example.ehu.animeckecker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.ehu.animeckecker.databinding.FragmentThisSeasenBinding
+import com.example.ehu.animeckecker.util.AppSharedPreferences
+
+class ThisSeasenFragment : Fragment() {
+
+    lateinit var binding: FragmentThisSeasenBinding
+    lateinit var token: String
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_this_seasen, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // tokenの取得
+        token = AppSharedPreferences(context!!).getToken()
+
+    }
+}
