cdb60a68c7cb21a74658c272e0f1f478e5bd7026
ehu <fehuehuf@gmail.com>
2019-07-09 14:34:20 +0900

    デバッグ用に秒で通知

--- app/src/main/java/com/example/ehu/animeckecker/NotificationEditFragment.kt
+++ app/src/main/java/com/example/ehu/animeckecker/NotificationEditFragment.kt
@@ -33,18 +33,22 @@ class NotificationEditFragment : Fragment() {
         binding.cancel.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_notificationEditFragment_to_thisSeasonFragment)
         }
+        binding.createDebug.setOnClickListener {
+            setAlarmDebug()
+            Navigation.findNavController(it).navigate(R.id.action_global_notificationEditFragment)
+        }
         return binding.root
     }
 
     private fun setAlarm() {
         // chipから時間を取得
         val notificationTime = mutableListOf<Int>()
-        for (i in 0 until binding.chipGroup.childCount) {
-            val chip = binding.chipGroup.getChildAt(i) as Chip
+        for (i in 0 until binding.chipGroupMinute.childCount) {
+            val chip = binding.chipGroupMinute.getChildAt(i) as Chip
             notificationTime.add(chip.tag.toString().toInt())
         }
-        for (i in 0 until binding.chipGroup2.childCount) {
-            val chip = binding.chipGroup2.getChildAt(i) as Chip
+        for (i in 0 until binding.chipGroupHour.childCount) {
+            val chip = binding.chipGroupHour.getChildAt(i) as Chip
             notificationTime.add(chip.tag.toString().toInt())
         }
         // 放送時刻を取得
@@ -54,7 +58,20 @@ class NotificationEditFragment : Fragment() {
         startedAt.set(Calendar.MINUTE, minute)
         startedAt.set(Calendar.MINUTE, 0)
         // alarmをセット
-        registerNotificationAlarm("ポケモン", 60, startedAt)
+        registerNotificationAlarm("ポケモン", notificationTime[0], startedAt)
+    }
+
+    private fun setAlarmDebug() {
+        val notificationTime = mutableListOf<Int>()
+        for (i in 0 until binding.chipGroupSecound.childCount) {
+            val chip = binding.chipGroupSecound.getChildAt(i) as Chip
+            notificationTime.add(chip.tag.toString().toInt())
+        }
+        val startedAt = Calendar.getInstance()
+        startedAt.timeInMillis = System.currentTimeMillis()
+        startedAt.add(Calendar.SECOND, notificationTime[0])
+        // alarmをセット
+        scheduleNotification("ポケモン", notificationTime[0].toString() + "秒", startedAt)
     }
 
     private fun registerNotificationAlarm(animeTitle: String, beforeSecond: Int, startedAt: Calendar) {
