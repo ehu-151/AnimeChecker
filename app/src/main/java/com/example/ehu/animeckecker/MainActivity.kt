01f32258a4076c2971006ebb1446d3b0f2c94dbe
ehu <fehuehuf@gmail.com>
2019-07-15 00:06:35 +0900

    toolbarの表示

--- app/src/main/java/com/example/ehu/animeckecker/MainActivity.kt
+++ app/src/main/java/com/example/ehu/animeckecker/MainActivity.kt
@@ -6,11 +6,23 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation.findNavController
 import com.example.ehu.animeckecker.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.toolbar.inflateMenu(R.menu.menu_main)
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.getItemId()) {
+                R.id.action_logout -> logout()
+            }
+            true
+        }
+    }
+
+    private fun logout() {
+
     }
 
     override fun onSupportNavigateUp() = findNavController(this, R.id.my_nav_host_fragment).navigateUp()
