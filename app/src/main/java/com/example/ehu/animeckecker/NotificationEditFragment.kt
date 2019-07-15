dfe55887a24fc09df866b6de16dbf9fe71619618
ehu <fehuehuf@gmail.com>
2019-07-15 23:48:12 +0900

    TimePickerの表示

--- app/src/main/java/com/example/ehu/animeckecker/NotificationEditFragment.kt
+++ app/src/main/java/com/example/ehu/animeckecker/NotificationEditFragment.kt
@@ -1,11 +1,17 @@
 package com.example.ehu.animeckecker
 
 
+import android.app.Dialog
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.Navigation
 import com.example.ehu.animeckecker.databinding.FragmentNotificationEditBinding
 import com.example.ehu.animeckecker.util.AnimeAlarmManager
@@ -33,6 +39,7 @@ class NotificationEditFragment : Fragment() {
 
         // isEdit共通
         binding = FragmentNotificationEditBinding.inflate(inflater, container, false)
+        binding.editText.setOnClickListener { showTimePicker() }
 
         // isEditによって、入力データ、遷移を分ける
         if (isEdit) {
@@ -205,5 +212,27 @@ class NotificationEditFragment : Fragment() {
 
     fun Int.zeroFill(): String {
         return String.format("%02d", this)
+    }
+
+    private fun showTimePicker() {
+        class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+            override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+                // Use the current time as the default values for the picker
+                val c = Calendar.getInstance()
+                val hour = c.get(Calendar.HOUR_OF_DAY)
+                val minute = c.get(Calendar.MINUTE)
+
+                // Create a new instance of TimePickerDialog and return it
+                return TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity))
+            }
+
+            override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+                val hour = String.format("%02d", hourOfDay)
+                val min = String.format("%02d", minute)
+                val timeText = "$hour:$min"
+                binding.editText.setText(timeText)
+            }
+        }
+        TimePickerFragment().show((activity as FragmentActivity).supportFragmentManager, "TAG")
     }
 }
