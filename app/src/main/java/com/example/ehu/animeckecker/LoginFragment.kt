190977ef3fcad065e64ad36f1d7b5b4ffebca7ed
ehu <fehuehuf@gmail.com>
2019-07-15 18:23:31 +0900

    fragmentの作成

---
+++ app/src/main/java/com/example/ehu/animeckecker/LoginFragment.kt
@@ -0,0 +1,22 @@
+package com.example.ehu.animeckecker
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.ehu.animeckecker.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    lateinit var binding: FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
