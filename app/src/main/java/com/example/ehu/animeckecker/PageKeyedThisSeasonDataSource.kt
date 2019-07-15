775ab1186d81ed3ab16f4541dbd38483e39831e2
ehu <fehuehuf@gmail.com>
2019-07-15 14:40:06 +0900

    DataSource.Factoryの修正

--- app/src/main/java/com/example/ehu/animeckecker/PageKeyedThisSeasonDataSource.kt
+++ app/src/main/java/com/example/ehu/animeckecker/PageKeyedThisSeasonDataSource.kt
@@ -1,22 +1,63 @@
 package com.example.ehu.animeckecker
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.paging.PageKeyedDataSource
 import com.example.ehu.animeckecker.remote.AnnictApiService
-import com.example.ehu.animeckecker.remote.AnnictWorksModel
+import com.example.ehu.animeckecker.remote.Works
+import com.example.ehu.animeckecker.util.Status
+import java.io.IOException
 
-class PageKeyedThisSeasonDataSource(private val api: AnnictApiService) : PageKeyedDataSource<Int, AnnictWorksModel>() {
-    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, AnnictWorksModel>) {
-        // コールバックに結果と前のページ、次のページを渡す
-//        callback.onResult(result, null, 2)
+class PageKeyedThisSeasonDataSource(
+    private val service: AnnictApiService,
+    private val token: String,
+    private val filterSeason: String
+) :
+    PageKeyedDataSource<Int, Works>() {
+
+    val networkState = MutableLiveData<Status<List<Works>>>()
+
+    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, Works>) {
+        callAPI(1, params.requestedLoadSize, token, filterSeason) { repos, next ->
+            callback.onResult(repos, null, next)
+        }
     }
 
-    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, AnnictWorksModel>) {
-        // コールバックに結果とその次のページを渡す
-//        callback.onResult(result, params.key + 1)
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Works>) {
+        callAPI(params.key, params.requestedLoadSize, token, filterSeason) { work, next ->
+            callback.onResult(work, next)
+        }
     }
 
-    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, AnnictWorksModel>) {
-        // コールバックに結果とその前のページを渡す
-//        callback.onResult(result, params.key - 1)
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Works>) {
+        // no
+    }
+
+    private fun callAPI(
+        page: Int,
+        perPage: Int,
+        token: String,
+        filterSeason: String,
+        callback: (work: MutableList<Works>, next: Int?) -> Unit
+    ) {
+        Log.d("app_thisseason", "page:$page, perPage$perPage")
+
+        networkState.postValue(Status.Logging)
+
+        try {
+            // 一覧を取得
+            val response =
+                service.getWorks(token, page = page, filterSeason = filterSeason, perPage = perPage).execute()
+
+            response.body()?.let {
+                var next: Int? = null
+                // nextPageがあるなら、インクリメント
+                if (it.nextPage != null) next = page + 1
+
+                callback(it.works, next)
+                networkState.postValue(Status.Success(it.works))
+            }
+        } catch (e: IOException) {
+        }
     }
 }
--- app/src/main/java/com/example/ehu/animeckecker/remote/ThisSeasonDataSourceFactory.kt
+++ app/src/main/java/com/example/ehu/animeckecker/remote/ThisSeasonDataSourceFactory.kt
@@ -3,8 +3,12 @@ package com.example.ehu.animeckecker.remote
 import androidx.paging.DataSource
 import com.example.ehu.animeckecker.PageKeyedThisSeasonDataSource
 
-class ThisSeasonDataSourceFactory(private val api: AnnictApiService) : DataSource.Factory<Int, AnnictWorksModel>() {
-    override fun create(): DataSource<Int, AnnictWorksModel> {
-        return PageKeyedThisSeasonDataSource(api)
+class ThisSeasonDataSourceFactory(
+    private val service: AnnictApiService,
+    private val token: String,
+    private val filterSeason: String
+) : DataSource.Factory<Int, Works>() {
+    override fun create(): DataSource<Int, Works> {
+        return PageKeyedThisSeasonDataSource(service, token, filterSeason)
     }
 }
