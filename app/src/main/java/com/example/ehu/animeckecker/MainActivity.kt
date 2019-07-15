a99e29a6e713161b046859f3b4e4329431708f90
ehu <fehuehuf@gmail.com>
2019-07-15 19:07:59 +0900

    logoutが上手くいかない

--- app/src/main/java/com/example/ehu/animeckecker/MainActivity.kt
+++ app/src/main/java/com/example/ehu/animeckecker/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.ehu.animeckecker
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
 import com.example.ehu.animeckecker.databinding.ActivityMainBinding
 import com.example.ehu.animeckecker.repository.LoginRepository
@@ -29,13 +29,7 @@ class MainActivity : AppCompatActivity() {
         LoginRepository().logout(token)
         AppSharedPreferences(this).setIsLogin(false)
         AppSharedPreferences(this).setToken("")
-        intentLogin()
-    }
-
-    private fun intentLogin() {
-        val intent = Intent(this@MainActivity, LoginActivity::class.java)
-            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        startActivity(intent)
+        Navigation.findNavController(binding.root).navigate(R.id.action_logout)
     }
 
     override fun onSupportNavigateUp() = findNavController(this, R.id.my_nav_host_fragment).navigateUp()
